89102d2b79eb138b8ca1fc1b0848ac8c4a2f124b
Andrew Rosen <andrew.rosen@temple.edu>
2021-10-26 15:37:17 -0400

    Update tetriswwithspacelasers.py

--- tetriswwithspacelasers.py
+++ tetriswwithspacelasers.py
@@ -1,1 +1,30 @@
-print("Hello, world!")
+import random
+def autoRollTo20():
+    total = 0
+    while total < 20:
+        roll = random.randint(1,6)
+        #print("Roll:", roll)
+        if roll == 1:
+            total = 0
+            #print("Turn total:", total)
+            return total
+        else:
+            total = total + roll 
+    #print("Turn total:", total)
+    return total
+
+
+def rollTo20Outcomes(numRuns):
+    outcomes = {}
+    for _ in range(numRuns):
+        score = autoRollTo20()
+        if score not in outcomes:
+            outcomes[score] = 1
+        else:
+            outcomes[score] += 1
+    return outcomes
+
+n = 10000
+outcomes = rollTo20Outcomes(n)
+for score in sorted(outcomes):
+    print(score, outcomes[score]/n ) 
